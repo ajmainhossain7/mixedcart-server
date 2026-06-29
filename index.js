d4ca45cc653edbb44e56ff729364328ccf10e64f
ajmainhossain7 <ajmainh0@gmail.com>
2026-06-29 12:36:52 +0600

    Connected with mongodb

---
+++ index.js
@@ -0,0 +1,24 @@
+const dns = require('node:dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+const expreess = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = expreess();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.use('/api/auth', require('./routes/authRoutes'));
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { registerUser, loginUser, logoutUser } = require('../controllers/authController');
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.post('/user', logoutUser);
+
+module.exports = router;

32ae3e5e053d131ed088d10676063d303a48de00
ajmainhossain7 <ajmainh0@gmail.com>
2026-07-01 18:27:12 +0600

    Update product api created

--- controllers/productController.js
+++ controllers/productController.js
@@ -38,3 +38,29 @@ const createProduct = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+
+const updateProduct = async (req, res) => {
+    try {
+        const { name, description, price, category, stock } = req.body;
+        const product = await Product.findById(req.params.id);
+        if (product) {
+            product.name = name || product.name;
+            product.description = description || product.description;
+            product.price = price || product.price;
+            product.category = category || product.category;
+            product.stock = stock || product.stock;
+            if (req.file) {
+                const result = await cloudinary.uploader.upload(req.file.path);
+                    console.log(result);
+                product.imageUrl = result.secure_url;
+            }
+            const updatedProduct = await product.save();
+            res.json(updatedProduct);
+        } else {
+            res.status(404).json({ message: 'Product not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
